8c8d1c4c7c4bd9013adfd12da64c138db647c713
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-10 19:46:51 -0400

    create migration templates

---
+++ db/migrate/20170910234627_create_addresses.rb
@@ -0,0 +1,6 @@
+class CreateAddresses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :addresses do |t|
+    end
+  end
+end
---
+++ db/migrate/20170910234636_create_image_to_items.rb
@@ -0,0 +1,6 @@
+class CreateImageToItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :image_to_items do |t|
+    end
+  end
+end
