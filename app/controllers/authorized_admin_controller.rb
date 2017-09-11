5b234e22349bd0ae2c64a0a1e8e53573faae6abd
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-11 14:55:26 -0400

    add authorized admin methods

--- app/controllers/authorized_admin_controller.rb
+++ app/controllers/authorized_admin_controller.rb
@@ -1,2 +1,10 @@
 class AuthorizedAdminController < ApplicationController
+  before_action :authorization
+
+  def authorization
+    if current_user && current_user.admin?
+    else
+      render file: 'public/404', layout: false, status: 404
+    end
+  end
 end
