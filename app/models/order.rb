e75cf634af3b9e37a3d21a00d52bca6673e9b974
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-11 09:12:07 -0400

    create order models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,11 @@
 class Order < ApplicationRecord
+  has_one :order_status
+  belongs_to :user
+
+  has_many :order_products,
+  class_name: "OrderProduct",
+  primary_key: :id,
+  foreign_key: :order_id
+
+  has_many :products, through: :order_products
 end
