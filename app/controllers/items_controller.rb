293f0e0de0618836b4cf098a83d2771e389cea2b
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-11 14:53:25 -0400

    add items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+  def index
+    if params[:search]
+      @items = Item.search(params[:search]).order('created_at DESC').paginate(page: params[:page], per_page: 16)
+    else
+      @items = Item.paginate(page: params[:page], per_page: 16)
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
