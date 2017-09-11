b40688f86e40e43b4887f99fc5f80cee8332d2f9
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-11 08:10:24 -0400

    add banner migration

--- db/migrate/20170911030536_create_frequently_bought_products.rb
+++ db/migrate/20170911030536_create_frequently_bought_products.rb
@@ -1,6 +1,10 @@
 class CreateFrequentlyBoughtProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :frequently_bought_products do |t|
+      t.integer :frequently_bought_together_id, null: false
+      t.integer :product_id, null: false
+
+      t.timestamps null: false
     end
   end
 end
