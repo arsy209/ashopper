57ebcc32a4a27a8bd5c9bf6a8d881839a6b2fd87
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-11 09:14:17 -0400

    create watched models

--- app/models/watched_list.rb
+++ app/models/watched_list.rb
@@ -1,2 +1,12 @@
 class WatchedList < ApplicationRecord
+  validates :user_id, presence: true
+
+  belongs_to :user
+
+  has_many :watched_products,
+  class_name: "WatchedProductsList",
+  primary_key: :id,
+  foreign_key: :watched_list_id
+
+  has_many :products, through: :watched_products
 end
--- app/models/watched_products_list.rb
+++ app/models/watched_products_list.rb
@@ -1,2 +1,11 @@
 class WatchedProductsList < ApplicationRecord
+  belongs_to :product,
+  class_name: "Product",
+  primary_key: :id,
+  foreign_key: :product_id
+
+  belongs_to :watched_list,
+  class_name: "WatchedList",
+  primary_key: :id,
+  foreign_key: :watched_list_id
 end
