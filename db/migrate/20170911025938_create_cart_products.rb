2393ac55f4bc1abb559766cb7cfa8aefa9e8a637
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-10 23:12:28 -0400

    add cart migrations

--- db/migrate/20170911025938_create_cart_products.rb
+++ db/migrate/20170911025938_create_cart_products.rb
@@ -1,6 +1,10 @@
 class CreateCartProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :cart_products do |t|
+      t.integer :cart_id, null: false
+      t.integer :product_id, null: false
+
+      t.timestamps null: false
     end
   end
 end
