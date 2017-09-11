c78a23bac648076d7f6653d10bbbb078fb503758
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-10 22:41:22 -0400

    add items migration

--- db/migrate/20170910234540_create_items.rb
+++ db/migrate/20170910234540_create_items.rb
@@ -1,6 +1,12 @@
 class CreateItems < ActiveRecord::Migration[5.1]
   def change
     create_table :items do |t|
+      t.string :title
+      t.text :descriptions
+      t.decimal :price
+      t.integer :inventory_status, default: 0
+
+      t.timestamps
     end
   end
 end
