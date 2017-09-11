a428f332da556f98be199ca49a83371e1870f80a
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-10 23:42:24 -0400

    add watch categories migration

--- db/migrate/20170911030318_create_watched_products.rb
+++
@@ -1,6 +0,0 @@
-class CreateWatchedProducts < ActiveRecord::Migration[5.1]
-  def change
-    create_table :watched_products do |t|
-    end
-  end
-end
--- db/migrate/20170911030326_create_watched_list_id_to_user.rb
+++
@@ -1,6 +0,0 @@
-class CreateWatchedListIdToUser < ActiveRecord::Migration[5.1]
-  def change
-    create_table :watched_list_id_to_users do |t|
-    end
-  end
-end
