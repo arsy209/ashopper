186e3b40ceb0eabaf30d24ec8e922c64667e5903
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-11 14:54:49 -0400

    add authorize user methods

--- app/controllers/authorized_user_controller.rb
+++ app/controllers/authorized_user_controller.rb
@@ -1,2 +1,10 @@
 class AuthorizedUserController < ApplicationController
+  before_action :authorization
+
+  def authorization
+    if current_user
+    else
+      render file: 'public/404', layout: false, status: 404
+    end
+  end
 end
