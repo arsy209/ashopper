39afc9cef096bc2d09aad1115ab10cab819bc170
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-11 11:35:05 -0400

    restart

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,2 @@
 Rails.application.routes.draw do
-  root "static_pages#root"
-  namespace :api, defaults: { format: :json } do
-    resource :session, only: [:create, :destroy]
-    resources :users, except: [:new] do
-      resources :addresses, only: [:create, :update]
-      resources :payments, only: [:create, :update, :destroy]
-    end
-    resources :orders, except: [:new, :destroy]
-    resources :cart_products, only: [:create, :update, :destroy]
-    resources :carts, only: [:show]
-    resources :storefronts, only: [:index]
-    resources :watched_products, only: [:index]
-    resources :frequently_bought_products, only: [:create]
-    get "carts/:id/delete_all", to: "carts#delete_all"
-    resources :products, except: [:index, :create, :new, :edit, :update, :show, :destroy] do
-        get "search", on: :collection
-    end
-    resources :categories, only: [:index, :show] do
-      resources :products, only: [:show]
-    end
-  end
 end
