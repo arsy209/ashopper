8491eafb37833541c59640b22c89940751146801
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-10 23:35:34 -0400

    add payments migration

--- db/migrate/20170911030144_create_creditcards.rb
+++ db/migrate/20170911030144_create_creditcards.rb
@@ -1,6 +1,9 @@
 class CreateCreditcards < ActiveRecord::Migration[5.1]
   def change
     create_table :creditcards do |t|
+      t.string :network, null: false
+
+      t.timestamps, null: false
     end
   end
 end
