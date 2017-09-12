10c1938db7cca104da11e02b39546bb68c5e9426
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-12 09:57:25 -0400

    add new home page things

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,9 @@ class HomeController < ApplicationController
     if params[:search]
       @items = Item.search(params[:search]).order('created_at DESC').paginate(page: params[:page], per_page: 16)
     else
-      @items = Item.paginate(page: params[:page], per_page: 16)
+      @items = Item.limit(2).order("RANDOM()")
+
     end
+
   end
 end
