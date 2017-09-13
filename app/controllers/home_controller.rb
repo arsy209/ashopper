6d40a3ba2666c3221f2decb8835fcc2761317c83
Arshdeep Singh <singh_arsy@yahoo.com>
2017-09-13 13:35:47 -0400

    add moew products

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,5 @@
 class HomeController < ApplicationController
   def index
-    if params[:search]
-      @items = Item.search(params[:search]).order('created_at DESC').paginate(page: params[:page], per_page: 16)
-    else
-      @items = Item.limit(2).order("RANDOM()")
-
-    end
-
+      @items = Item.limit(4).order("RANDOM()")
   end
 end
